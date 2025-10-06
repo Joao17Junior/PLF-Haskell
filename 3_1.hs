96789cbb5f0f77d63990a6e8159961971b757ba1
Joao Rodrigues Alberton Junior <up202306719@up.pt>
2025-10-06 12:26:18 +0100

    Add files via upload

--- 3_1.hs
+++ 3_1.hs
@@ -15,6 +15,12 @@ myReplicate 0 a = []
 myReplicate i a = [a] ++ myReplicate (i-1) a
 
 myIndex :: [a] -> Int -> a
-myIndex [] x = undefined
-myIndex (x1:ls) x   | x > 0 = myIndex ls (x-1)
+myIndex [] x = error "Out of Bounds"
+myIndex (x1:ls) x   | x < 0 = error "Negative Index"
+                    | x > 0 = myIndex ls (x-1)
                     | otherwise = x1
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem x [] = False
+myElem x (x1:ls)    | x == x1 = True
+                    | otherwise = myElem x ls
